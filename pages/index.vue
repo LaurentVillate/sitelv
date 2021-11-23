e6143cfa8fb3233113a5122df24780a185b2a178
Laurent Villate <laurent.villate@yahoo.com>
2021-11-23 11:58:41 +0100

    essai remplacement / par ~

--- pages/index.vue
+++ pages/index.vue
@@ -22,8 +22,8 @@ export default {
     data () {
       return{
           galeries: [
-            {image: "galerie/hue_Autoportrait.png", lien:"/details/autoportrait", alt:"En tong sur un terrain vague à Hué"},
-            {image: "/galerie/tokyo_Meiji.png", lien:"/details/tokyo", alt:"Vue du Meiji-Jingu dans le parc de Yoyogi à Tokyo. Au loin, le le NTT Docomo Yoyogi Building."},
+            {image: "~galerie/hue_Autoportrait.png", lien:"/details/autoportrait", alt:"En tong sur un terrain vague à Hué"},
+            {image: "galerie/tokyo_Meiji.png", lien:"/details/tokyo", alt:"Vue du Meiji-Jingu dans le parc de Yoyogi à Tokyo. Au loin, le le NTT Docomo Yoyogi Building."},
             {image: "/galerie/phnompenh_Stade.png", lien:"/details/phnompenh1", alt:"Un match de foot devant le palais des Sports, dans le complexe du Stade Olympique de Phnom Penh (architecte: Vann Molyvann)."},
             {image: "/galerie/saigon_Moto.png", lien:"/details/saigon", alt:"Une moto devant un mur jaune et des drapeaux rouges à Saigon Ho Chi Minh."},
             {image: "/galerie/versailles_Chateau.png", lien:"/details/versailles", alt:"Vue de nuit du chateau de Versailles."},
