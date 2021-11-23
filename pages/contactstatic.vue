2aa7503cbce2c5cb104c38cbbce95de781483d67
Laurent Villate <laurent.villate@yahoo.com>
2021-11-23 16:52:14 +0100

    suite formstatic

--- pages/contactstatic.vue
+++ pages/contactstatic.vue
@@ -14,14 +14,45 @@
       class="pt-16 px-sm-16"
     >
       <!--Formulaire de contact-->
-      <div id="contact-form">
-                <form>
-                <input type="hidden" name="_subject" value="Message de laurentvillate.com" />
-                <input type="email" name="_replyto" :placeholder="$t('form.mail')" required>
-                <textarea name="message" :placeholder="$t('form.message')" required></textarea>
-                <button type="submit">Send</button>
-            </form>
-        </div>
+      <div>
+                <v-form>
+                    <v-container>
+                <v-text-field type="hidden" name="_subject" value="Message de laurentvillate.com"></v-text-field>
+                <v-row class="py-sm-16">
+                <v-col cols="12">
+                <v-text-field 
+                class="textgrey"
+                type="email" 
+                name="_replyto" 
+                :placeholder="$t('form.mail')" 
+                required
+                ></v-text-field>
+                </v-col>
+                </v-row>
+                <v-row class="py-sm-16">
+                <v-col cols="12">
+                <v-textarea 
+                class="textgrey"
+                outlined
+                name="message" 
+                :placeholder="$t('form.message')" 
+                required
+                ></v-textarea>
+                </v-col>>
+            </v-row>
+            <v-row class="py-sm-16">
+            <v-col cols="12">
+                <v-btn 
+                elevation="0"
+                color="blue-grey darken-4"
+                class="textorange mx-6"
+                type="submit"
+                >Send</v-btn>
+            </v-col>
+            </v-row>
+                </v-container>
+            </v-form>
+            </div>
     </v-card>
     <v-divider color="#B3E5FC"></v-divider>
   </div>
