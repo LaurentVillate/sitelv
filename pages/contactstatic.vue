5183b264a5d4e1bafc424280448f64d7386d4801
Laurent Villate <laurent.villate@yahoo.com>
2021-11-24 11:37:43 +0100

    suite form contact pour github-pages

--- pages/contactstatic.vue
+++ pages/contactstatic.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-     <!--Ligne de séparation avec l'en-tête présente seulement sur les pages bleu clair-->
+    <!--Ligne de séparation avec l'en-tête présente seulement sur les pages bleu clair-->
     <v-divider color="#B3E5FC"></v-divider>
     <!--Bouton de retour à la page d'accueil-->
     <v-card tile flat color="blue lighten-5">
@@ -15,51 +15,75 @@
     >
       <!--Formulaire de contact-->
       <div>
-                <v-form>
-                    <v-container>
-                <v-text-field type="hidden" name="_subject" value="Message de laurentvillate.com"></v-text-field>
-                <v-row class="py-sm-16">
-                <v-col cols="12">
-                <v-text-field 
-                class="textgrey"
-                type="email" 
-                name="_replyto" 
-                :placeholder="$t('form.mail')" 
-                required
+        <v-form>
+          <v-container>
+            <v-text-field
+              solo
+              flat
+              type="hidden"
+              color="transparent"
+              name="_subject"
+              value="Message de laurentvillate.com"
+            ></v-text-field>
+            <v-row class="pa-sm-16">
+              <v-col cols="12">
+                <v-text-field
+                  class="textgrey"
+                  type="email"
+                  name="_replyto"
+                  :placeholder="$t('form.mail')"
+                  required
                 ></v-text-field>
-                </v-col>
-                </v-row>
-                <v-row class="py-sm-16">
-                <v-col cols="12">
-                <v-textarea 
-                class="textgrey"
-                outlined
-                name="message" 
-                :placeholder="$t('form.message')" 
-                required
+              </v-col>
+            </v-row>
+            <v-row class="pa-sm-16">
+              <v-col cols="12">
+                <v-textarea
+                  class="textgrey"
+                  outlined
+                  name="message"
+                  :placeholder="$t('form.message')"
+                  required
                 ></v-textarea>
-                </v-col>>
+              </v-col>
             </v-row>
-            <v-row class="py-sm-16">
-            <v-col cols="12">
-                <v-btn 
-                elevation="0"
-                color="blue-grey darken-4"
-                class="textorange mx-6"
-                type="submit"
-                >Send</v-btn>
-            </v-col>
+            <v-row class="pa-sm-16">
+              <v-col cols="12">
+                <v-btn
+                  elevation="0"
+                  color="blue-grey darken-4"
+                  class="textorange mx-6"
+                  type="submit"
+                  >{{ $t("form.envoi") }}
+                </v-btn>
+                <v-btn
+                  elevation="0"
+                  color="blue-grey darken-4"
+                  class="textorange"
+                  @click="cancel"
+                >
+                  {{ $t("form.annulation") }}
+                </v-btn>
+              </v-col>
             </v-row>
-                </v-container>
-            </v-form>
-            </div>
+          </v-container>
+        </v-form>
+      </div>
     </v-card>
     <v-divider color="#B3E5FC"></v-divider>
   </div>
 </template>
 
 <script>
-
+export default {
+methods: {
+    
+    //Fonction cancel: Retour à l'index//
+    cancel() {
+      this.$router.go(-1);
+    },
+  },
+}
 </script>
 
 <style scoped>
